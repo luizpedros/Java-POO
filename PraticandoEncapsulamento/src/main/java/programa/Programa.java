d3af1ed0cbc9fbf366e4d74ac2e08405f2847e12
Luiz Pedro <78607909+luizpedros@users.noreply.github.com>
2021-04-12 14:22:01 -0300

    Add files via upload

---
+++ PraticandoEncapsulamento/src/main/java/programa/Programa.java
@@ -0,0 +1,16 @@
+package programa;
+
+import classes.ControleRemoto;
+
+public class Programa {
+    public static void main(String[] args){
+
+        ControleRemoto c1 = new ControleRemoto();
+
+
+
+        c1.abrirMenu();
+
+
+    }
+}
